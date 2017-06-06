162fb00bf0a655de372b862929e9e483efac07f4
logonrm <logonrm@fiap.com.br>
2017-06-05 22:46:13 -0300

    app

--- src/br/com/fiap/App.java
+++ src/br/com/fiap/App.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.JOptionPane;
 
 import br.com.fiap.dao.Helper;
 import br.com.fiap.entity.Loja;
@@ -19,8 +20,8 @@ public class App {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("DBComparador");
 		EntityManager em = emf.createEntityManager();
 		incluirLoja(em);
-		listarLoja(em);
-		buscarLoja(em, "2000");
+//		listarLoja(em);
+//		buscarLoja(em, "2000");
 	}
 	
 	private static void incluirLoja(EntityManager em){
@@ -28,42 +29,44 @@ public class App {
 		Loja loja = new Loja();
 		
 		Oferta oferta = new Oferta();
-		oferta.setPreco(30.00d);
-		oferta.setAtivo(true);
-		Set<Oferta> ofertas = new HashSet<>();
+		Set<Oferta> ofertas = new HashSet<>();		
+		Produto prod = new Produto();
+		Set<Produto> ps = new HashSet<>();
+		
+		String l = JOptionPane.showInputDialog("Digite uma loja: ");
+		loja.setNome(l);
+		String p = JOptionPane.showInputDialog("Digite um produto: ");
+		String pr = JOptionPane.showInputDialog("Digite seu preço: ");
+		String at = JOptionPane.showInputDialog("Digite 1 se ativa ou 0 se inativa:");
+		oferta.setPreco(Double.valueOf(pr));
+		oferta.setAtivo(Boolean.valueOf(at));
 		ofertas.add(oferta);
 		
-		Produto prod = new Produto();
-		prod.setNome("Moto z");
 		prod.setOfertas(ofertas);
-		Set<Produto> ps = new HashSet<>();
-		ps.add(prod);		
+		ps.add(prod);	
 		
-		loja.setNome("MOTOROLA");
-		loja.setProdutos(ps);
-		
-		try {
-			dao.salvar(funcionario);
-			System.out.println("Funcionario OK");
-		} 
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+//		try {
+//			dao.salvar(funcionario);
+//			System.out.println("Funcionario OK");
+//		} 
+//		catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
 	}
-	private static void listarFuncionarios(EntityManager em){
-		Helper dao = new Helper(em);
-		List<Funcionario> funcionarios = dao.listarTodos();
-		for (Funcionario funcionario : funcionarios) {
-			System.out.println(funcionario.getMatricula() + ": " +
-					funcionario.getAtivo());
-		}
-		em.close();
-	}
-	private static void buscarFuncionario(EntityManager em, String
-			matricula){
-		Helper dao = new Helper(em);
-		Funcionario f = dao.buscarFuncionario(matricula);
-		System.out.println(f.getMatricula() + ": " + f.getAtivo());
-	}
+//	private static void listarFuncionarios(EntityManager em){
+//		Helper dao = new Helper(em);
+//		List<Funcionario> funcionarios = dao.listarTodos();
+//		for (Funcionario funcionario : funcionarios) {
+//			System.out.println(funcionario.getMatricula() + ": " +
+//					funcionario.getAtivo());
+//		}
+//		em.close();
+//	}
+//	private static void buscarFuncionario(EntityManager em, String
+//			matricula){
+//		Helper dao = new Helper(em);
+//		Funcionario f = dao.buscarFuncionario(matricula);
+//		System.out.println(f.getMatricula() + ": " + f.getAtivo());
+//	}
 
 }
