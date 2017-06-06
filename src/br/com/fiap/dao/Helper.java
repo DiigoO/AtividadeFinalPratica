fbd6b613686d4ae8b002d2cb9dfb069b2399cd0f
logonrm <logonrm@fiap.com.br>
2017-06-05 21:59:52 -0300

    helper e lojas

--- src/br/com/fiap/dao/Helper.java
+++ src/br/com/fiap/dao/Helper.java
@@ -2,6 +2,11 @@ package br.com.fiap.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.fiap.entity.Loja;
+
 public class Helper {
 	private EntityManager em;
 
@@ -9,10 +14,10 @@ public class Helper {
 		this.em = em;
 	}
 
-	public void salvar(Loja funcionario) throws Exception { 
+	public void salvar(Object loja) throws Exception { 
 		try {
 			em.getTransaction().begin(); 
-			em.persist(funcionario); 
+			em.persist(loja); 
 			em.getTransaction().commit();
 		} 
 		catch (Exception e) {
@@ -22,48 +27,21 @@ public class Helper {
 	
 	//JPQL: Usando Query
 	@SuppressWarnings("unchecked")
-	public List<Funcionario> listarFuncionarios(){
-		Query query = em.createQuery("select f from Funcionario f"); 
+	public List<Loja> listarlojas(){
+		Query query = em.createQuery("select * from Lojas"); 
 		return query.getResultList();
 		
-		//Alternativa
-		//TypedQuery<Funcionario> tQuery = em.createQuery("select f from Funcionario f", Funcionario.class); 
-		//return tQuery.getResultList();
 	}
 
-	public Funcionario buscarFuncionario(String numMatricula){
-		Query query = em.createQuery("select f from Funcionario f where matricula = :matricula");
-		query.setParameter("matricula", numMatricula); 
-		Funcionario f = (Funcionario) query.getSingleResult(); 
-		return f;
-		
-		//Alternativa
-		
-		//TypedQuery<Funcionario> tQuery = em.createQuery("select f from Funcionario f where matricula = :matricula", Funcionario.class);
-		//tQuery.setParameter("matricula", numMatricula); 
-		//return tQuery.getSingleResult(); 
-		
+	public Loja buscarLoja(String lojaId){
+		Query query = em.createQuery("select * from Loja where lojaId = :lojaId");
+		query.setParameter("lojaId", lojaId); 
+		Loja l = (Loja) query.getSingleResult(); 
+		return l;		
 	}
 	
-	public Funcionario buscarFuncionario(int id){
-		Funcionario funcionario = this.em.find(Funcionario.class, id);
-		return funcionario;
+	public Loja buscarLoja(int id){
+		Loja loja = this.em.find(Loja.class, id);
+		return loja;
 	}
-	
-	public Tarefa buscarTarefa(int id){
-		Tarefa tarefa = this.em.find(Tarefa.class, id);
-		return tarefa;
-	}
-	
-	//JPQL: usando NamedQuery
-	@SuppressWarnings("unchecked")
-	public List<Funcionario> listarTodos(){
-		Query query = em.createNamedQuery("Funcionario.findAll");
-		return query.getResultList();
-		
-		//Alternativa
-		//TypedQuery<Funcionario> tQuery = em.createNamedQuery("Funcionario.findAll", Funcionario.class); 
-		//return tQuery.getResultList();
-		
-	} 
 }
